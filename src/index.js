4dfcc1c84a5ffd05b77e935a7bd4fcdc42421851
Chuvetharani <68391363+Chuvetharani@users.noreply.github.com>
2020-12-18 23:08:45 +0530

    srabc

--- src/index.js
+++ src/index.js
@@ -7,12 +7,113 @@ const port = 8080
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 const { connection } = require('./connector')
-
-
-
-
-
-
-app.listen(port, () => console.log(`App listening on port ${port}!`))
-
-module.exports = app;
+app.get("/totalRecovered", (req, res) => {
+    connection
+      .aggregate([
+        {
+          $group: {
+            _id: "total",
+            recovered: {
+              $sum: "$recovered",
+            },
+          },
+        },
+      ])
+      .exec()
+      .then((result) => {
+        (result._doc_id = "total"), res.send({ data: result[0] });
+      });
+  });
+  
+  app.get("/totalActive", (req, res) => {
+    connection
+      .aggregate([
+        {
+          $group: {
+            _id: "total",
+            active: {
+              $sum: {
+                $subtract: ["$infected", "$recovered"],
+              },
+            },
+          },
+        },
+      ])
+      .exec()
+      .then((result) => {
+        (result._doc_id = "total"), res.send({ data: result[0] });
+      })
+      .catch((err) => console.log(err));
+  });
+  
+  app.get("/totalDeath", (req, res) => {
+    connection
+      .aggregate([
+        {
+          $group: {
+            _id: "total",
+            death: {
+              $sum: "$death",
+            },
+          },
+        },
+      ])
+      .exec()
+      .then((result) => {
+       res.send({ data: result[0] });
+      });
+  });
+  
+  app.get(`/hotspotStates`, (req, res) => {
+    connection
+      .aggregate([
+        {
+          $project: {
+            _id: 0,
+            state: "$state",
+            rate: {
+              $round: [
+                { $subtract: [1, { $divide: ["$recovered", "$infected"] }] },
+                5,
+              ],
+            },
+          },
+        },
+        {
+          $match: {
+            rate: { $gt: 0.1 },
+          },
+        },
+      ])
+      .exec()
+      .then((result) => {
+        res.send({ data: result });
+      })
+      .catch((err) => console.log("sm error....", err));
+  });
+  
+  app.get("/healthyStates", (req, res) => {
+    connection
+      .aggregate([
+        {
+          $project: {
+            _id: 0,
+            state: "$state",
+            mortality: { $round: [{ $divide: ["$death", "$infected"] }, 5] },
+          },
+        },
+        {
+          $match: {
+            mortality: { $lt: 0.005 },
+          },
+        },
+      ])
+      .exec()
+      .then((result) => {res.send({ data: result });
+      })
+      .catch((err) => console.log("sm error....", err));
+  });
+  
+  app.listen(port, () => console.log(`App listening on port ${port}!`));
+  
+  module.exports = app;
